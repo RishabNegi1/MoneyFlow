a337a060faf4a3cfaceceaa44a453e8426d1fb2b
RishabNegi1 <rishabnegi108@gmail.com>
2023-04-05 15:56:02 +0530

    Et

--- lib/Pages/signup.dart
+++ lib/Pages/signup.dart
@@ -4,6 +4,7 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white,
       child: Column(
         children: [Image.asset("/assets/images/sign.png")],
       )
